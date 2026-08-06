9e231e8b811f724cdcbc12e03c276c9055eb1a84
jw-underwood-47 <jwunderwood47@gmail.com>
2026-08-06 15:23:03 -0400

    update factor_count to handle 1-2 arguments

--- factor_counts/factor_count.c
+++ factor_counts/factor_count.c
@@ -13,6 +13,17 @@ int main(int argc, char*argv[]){
     int temp = 0;
     int count_even = 0;
     int count_odd = 0;
+    if (argc <= 1){
+        printf("Error: not enough arguments\n");
+        exit(-1);
+    }
+    else if (argc == 2){
+        temp = factor_count(atoi(argv[1]));
+        printf("%d factors\n", temp);
+        /* curiously, if ", temp" is removed the output printed
+         * is atoi(argv[1]), at least on my system */
+        exit(0);
+    }
     for (int i = atoi(argv[1]); i <= atoi(argv[2]); i++){
         temp = factor_count(i);
         printf("%d: %d factors\n", i, temp);
